ae6642ff18203f0181430ad2d4db55fb60333e2e
Grenda Carla <grendacarlaferreira@gmail.com>
2021-03-10 19:17:06 -0300

    primeiro comit

---
+++ src/Gerenciador_De_Estoque/Main.java
@@ -0,0 +1,16 @@
+package Gerenciador_De_Estoque;
+
+import javax.swing.JFrame;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        TelaAdministrador Janela = new TelaAdministrador();
+        Janela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        Janela.show();
+    }
+    
+}
